ac33577d4a149d439e6f800efa045976d0799ea2
Adi <oradam86@gmail.com>
2022-07-19 13:30:36 +0200

    Made changes for "this." keyword(s)

--- lib/question.dart
+++ lib/question.dart
@@ -1,16 +1,18 @@
+// ignore_for_file: prefer_initializing_formals
+
 class Question {
   String questionText = "";
   bool questionAnswer = true;
-
-// It's called CONSTRUCTOR
+/*
+  It's called CONSTRUCTOR
   Question({required this.questionText, required this.questionAnswer});
+*/
 /*
   Same as above, but longer version:
   It's called CONSTRUCTOR
-
-  Question({required String q, required bool a}) {
-    questionText = q;
-    questionAnswer = a;
-  }
 */
+  Question({required String questionText, required bool questionAnswer}) {
+    this.questionText = questionText;
+    this.questionAnswer = questionAnswer;
+  }
 }
