27ed7f30faedbe883940cd671274448a191762de
Adi <oradam86@gmail.com>
2022-07-19 13:04:53 +0200

    Constructor [shorter, longer]

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,15 @@ class Question {
   String questionText = "";
   bool questionAnswer = true;
 
+// It's called CONSTRUCTOR
+  Question({required this.questionText, required this.questionAnswer});
+/*
+  Same as above, but longer version:
+  It's called CONSTRUCTOR
+
   Question({required String q, required bool a}) {
     questionText = q;
     questionAnswer = a;
   }
+*/
 }
